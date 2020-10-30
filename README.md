fcbdebf9ccb3932c51450f920e07a37f594c867c
Kornpob Bhirombhakdi <kbhirombhakdi@altalune.local>
2020-10-30 12:40:33 -0400

    v.1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mag2flux
-mag2flux converts AB magnitude to flux density (erg cm-2 s-1).
+# HeaderSummary
+HeaderSummary does retrieving information from fits files' headers. path_list provides the search paths applied by glob.glob(). For each file, astropy.io.fits.open() is used to open the file. Header info is retrieved as specified in keywords.
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="mag2flux",
+    name="headersummary",
     version="1.0.0",
     author="Kornpob Bhirombhakdi",
     author_email="kbhirombhakdi@stsci.edu",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/bkornpob/mag2flux",
+    url="https://github.com/bkornpob/headersummary",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
